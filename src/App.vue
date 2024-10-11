ad86824084d7da79c6e87c94be5ee64f993afbc5
evasyukov <egor.vasyukov1@gmail.com>
2024-10-11 18:21:34 +0700

    добавлены: - заголовок, описание - кнопка - json файл с данными

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,39 @@
 <template>
+  <div class="title">
+    <div class="title__text">Что поесть?</div>
 
+    <div class="title__description">
+      <p>
+        Сервис помогает вам выбрать то, что поесть, когда у вас закончились
+        идеи.
+      </p>
+      <p>
+        Тыкаете на кнопку - сайт предлагает вам блюдо и советует рецепт
+      </p>
+    </div>
+  </div>
+
+  <RecipeSelection />
 </template>
 
 <script setup>
-
+import RecipeSelection from "./components/RecipeSelection.vue"
 </script>
 
 <style scoped lang="scss">
+.title {
+  &__text {
+    font-size: 6rem;
+  }
 
+  &__description {
+    display: grid;
+    justify-items: start;
+    gap: 20px;
+    p {
+      font-size: 16px;
+      margin: 0;
+    }
+  }
+}
 </style>
---
+++ src/components/RecipeSelection.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="recipe-selection">
+    <div class="recipe-selection__button">
+      <button>Показать рецепт</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineComponent } from "vue"
+
+defineComponent({
+  name: "RecipeSelection",
+})
+</script>
+
+<style scoped lang="scss">
+.recipe-selection {
+  margin: 40px 0;
+
+  &__button {
+    display: flex;
+    justify-content: center;
+
+    button {
+      font-size: 60px;
+      padding: 40px 20px;
+      text-decoration: 3px underline;
+      border: 2px solid #00ffa6;
+      border-radius: 12px;
+      background-color: transparent;
+      color: #00ffa6;
+      cursor: pointer;
+      transition: background-color 0.7s;
+
+      &:hover {
+        background-color: #00ffa61f;
+        // color: white;
+      }
+    }
+  }
+}
+</style>
